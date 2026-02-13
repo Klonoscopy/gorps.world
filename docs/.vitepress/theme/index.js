dc6ff4bdc0b2cb2e4e7e2d079f4c9db37b2063b9
Klonoscopy <140009102+Klonoscopy@users.noreply.github.com>
2026-02-13 02:18:45 -0700

    fah

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,16 +4,12 @@ import './default.css'
 import './wiki.css'
 
 import StoreBadge from '../../components/StoreBadge.vue'
-import InfoModal from '../../components/InfoModal.vue'
-import WikiEntry from '../../components/WikiEntry.vue'
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('StoreBadge', StoreBadge)
-    app.component('InfoModal', InfoModal)
-    app.component('WikiEntry', WikiEntry)
   },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
